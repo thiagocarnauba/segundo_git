959d28394e00461002303e1716833c5cdefd3e4e
thiagocarnauba <thicooliveiracarnauba@gmail.com>
2024-03-19 11:41:30 -0300

    Adicionado a função decoradora

--- Funcs.py
+++ Funcs.py
@@ -1,5 +1,25 @@
-def exibir(nome):
-    for l in nome:
-        print(l)
+"""
+Funções Decoradoras
 
-exibir('Thiago Oliveira')
+Primeiro exemplo, decora um função simples de exibir o nome depois adiciona um sobrenome
+"""
+def decoradora(funcao):
+    def interna(nome):
+        nome_alterado = nome + " Oliveira"
+        return funcao(nome_alterado)
+
+    return interna
+
+
+def exibir_nome(nome):
+    print(nome)
+
+#função normal
+exibir_nome('Thiago')
+
+#criando função decoradora
+decorada = decoradora(exibir_nome)
+
+#função decoradora com sobrenome
+decorada('Thiago')
+decorada('Marcos')
